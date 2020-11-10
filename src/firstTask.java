7b96e022d2bf24a059bac1c6138cbdf67c6aae65
Aliyaguliyeva <aliya.1988@gmail.com>
2020-11-10 14:45:10 -0500

    Ignore

---
+++ src/firstTask.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class firstTask {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("It is my first private task");
+
+
+
+
+    }
+}
